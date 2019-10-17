10d6961c3a0b058f7fca8c219567cc2d525a3e52
unknown <developer@grupodpg.com.br>
2019-10-17 19:24:33 -0300

    subindo ajustes move lista

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,12 +1,14 @@
-import React, { useRef } from 'react';
+import React, { useRef, useContext } from 'react';
 import { useDrag, useDrop } from 'react-dnd';
+import BoardContext from '../Board/context';
 import { Container, Label } from './styles';
 
-export default function Card({ data, index }) {
+export default function Card({ data, index, listIndex }) {
   const ref = useRef();
+  const { move } = useContext(BoardContext);
 
   const [{ isDragging }, dragRef] = useDrag({
-    item: { type: 'CARD', index },
+    item: { type: 'CARD', index, listIndex },
     collect: monitor => ({
       isDragging: monitor.isDragging(),
     }),
@@ -15,10 +17,13 @@ export default function Card({ data, index }) {
   const [, dropRef] = useDrop({
     accept: 'CARD',
     hover(item, monitor) {
+      const draggedListIndex = item.listIndex;
+      const targetListIndex = listIndex;
+
       const draggedIndex = item.index;
       const targetIndex = index;
 
-      if (draggedIndex === targetIndex) {
+      if (draggedIndex === targetIndex && draggedListIndex === targetListIndex) {
         return;
       }
 
@@ -35,6 +40,11 @@ export default function Card({ data, index }) {
       if (draggedIndex > targetIndex && draggedTop > targetCenter) {
         return;
       }
+
+      move(draggedListIndex, targetListIndex, draggedIndex, targetIndex);
+
+      item.index = targetIndex;
+      item.listIndex = targetListIndex;
     }
   })
 
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -3,7 +3,7 @@ import { MdAdd } from 'react-icons/md';
 import Card from '../Card';
 import { Container } from './styles';
 
-export default function List({ data }) {
+export default function List({ data, index: listIndex }) {
   return (
     <Container done={data.done}>
       <header>
@@ -16,7 +16,12 @@ export default function List({ data }) {
       </header>
 
       <ul>
-        {data.cards.map((card, index) => <Card key={card.id} index={index} data={card} />)}
+        {data.cards.map((card, index) => (
+          <Card
+            key={card.id}
+            listIndex={listIndex}
+            index={index}
+            data={card} />))}
       </ul>
     </Container>
   );
